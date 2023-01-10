3f3b9727a20467b5f756d37be90243c6e812e2e0
georgeakhmedov <goshlolka@gmail.com>
2023-01-10 18:39:27 +0300

    commit from mac day 17

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,27 @@
 package day17;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static day17.ChessPiece.PAWN_BLACK;
+import static day17.ChessPiece.PAWN_WHITE;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<ChessPiece> chesses = new ArrayList<>(Arrays.asList());
+
+        int counter = 0;
+
+        while(counter < 0){
+            chesses.add(PAWN_WHITE);
+            chesses.add(PAWN_BLACK);
+            counter++;
+        }
+
+        for(ChessPiece piece : chesses) {
+            System.out.println(piece);
+        }
 
     }
 }
